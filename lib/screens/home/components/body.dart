5585068f16d61bb797ae46208d2bbb17043f2b65
João Vitor <jvmr535@gmail.com>
2023-06-23 19:08:10 -0300

    Categories

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:online_shop_app/constants.dart';
+
+import 'categories.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+          child: Text("Woman",
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge
+                  ?.copyWith(fontWeight: FontWeight.bold)),
+        ),
+        const Categories()
+      ],
+    );
   }
 }
